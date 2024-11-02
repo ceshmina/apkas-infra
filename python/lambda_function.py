af42ee4da59e0f400e157f99b95c3f784a0d0bc1
ceshmina <shu@apkas.net>
2024-11-02 10:47:14 +0900

    画像リサイズのルール変更

--- python/lambda_function.py
+++ python/lambda_function.py
@@ -19,23 +19,6 @@ def resize(input_path: str, output_path: str, size: int):
     resized.save(output_path)
 
 
-def crop_center(input_path: str, output_path: str):
-    image = Image.open(input_path)
-    width, height = image.size
-    if width > height:
-        new_height = height
-        new_width = height
-    else:
-        new_width = width
-        new_height = width
-    left = (width - new_width) // 2
-    right = left + new_width
-    top = (height - new_height) // 2
-    bottom = top + new_height
-    cropped = image.crop((left, top, right, bottom))
-    cropped.save(output_path)
-
-
 def extract_exif(input_path: str, output_path: str):
     image = Image.open(input_path)
     exif_raw = image._getexif()
@@ -67,25 +50,20 @@ def lambda_handler(event, context):
 
     large_path = f'/tmp/{filename}_large.webp'
     medium_path = f'/tmp/{filename}_medium.webp'
-    small_path = f'/tmp/{filename}_small.webp'
     thumbnail_path = f'/tmp/{filename}_thumbnail.webp'
     exif_path = f'/tmp/{filename}_exif.json'
 
-    resize(original_path, large_path, 4096)
-    resize(original_path, medium_path, 2048)
-    resize(original_path, small_path, 1024)
-    crop_center(original_path, thumbnail_path)
-    resize(thumbnail_path, thumbnail_path, 256)
+    resize(original_path, large_path, 3840)
+    resize(original_path, medium_path, 1920)
+    resize(original_path, thumbnail_path, 240)
     extract_exif(original_path, exif_path)
 
     large_key = key.replace('original', 'large').replace('.jpg', '.webp')
     medium_key = key.replace('original', 'medium').replace('.jpg', '.webp')
-    small_key = key.replace('original', 'small').replace('.jpg', '.webp')
     thumbnail_key = key.replace('original', 'thumbnail').replace('.jpg', '.webp')
     exif_key = key.replace('original', 'exif').replace('.jpg', '.json')
 
     s3_client.upload_file(large_path, bucket, large_key)
     s3_client.upload_file(medium_path, bucket, medium_key)
-    s3_client.upload_file(small_path, bucket, small_key)
     s3_client.upload_file(thumbnail_path, bucket, thumbnail_key)
     s3_client.upload_file(exif_path, bucket, exif_key)
--- python/test_local.py
+++ python/test_local.py
@@ -1,15 +1,13 @@
-from lambda_function import resize, crop_center, extract_exif
+from lambda_function import resize, extract_exif
 
 
 def main():
-    resize('sample/01_original.jpg', 'sample/01_medium.webp', 2048)
-    crop_center('sample/01_original.jpg', 'sample/01_thumbnail.webp')
-    resize('sample/01_thumbnail.webp', 'sample/01_thumbnail.webp', 256)
+    resize('sample/01_original.jpg', 'sample/01_medium.webp', 1920)
+    resize('sample/01_original.jpg', 'sample/01_thumbnail.webp', 240)
     extract_exif('sample/01_original.jpg', 'sample/01_exif.json')
 
-    resize('sample/02_original.jpg', 'sample/02_large.webp', 4096)
-    crop_center('sample/02_original.jpg', 'sample/02_thumbnail.webp')
-    resize('sample/02_thumbnail.webp', 'sample/02_thumbnail.webp', 256)
+    resize('sample/02_original.jpg', 'sample/02_large.webp', 3840)
+    resize('sample/02_original.jpg', 'sample/02_thumbnail.webp', 240)
 
 
 if __name__ == '__main__':
